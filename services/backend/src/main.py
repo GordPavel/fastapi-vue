0a41eb14f7f0e89753e6beeaed2e4053a4f86cd7
GordPavel <pvgord@icloud.com>
2021-12-08 02:50:59 +0300

    fix plotter

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -29,26 +29,34 @@ async def solve_equation(request: Request, a: float = 0, b: float = 0, c: float 
 
 def get_plot_encoded(a: float = 0, b: float = 0, c: float = 0):
     solved_roots = np.roots([a, b, c])
-    roots = np.sort(solved_roots[np.isreal(solved_roots)]).tolist()
-    if len(roots) == 0:
-        return
-    elif len(roots) == 2:
+    roots = list(np.sort(np.unique(solved_roots[abs(np.imag(solved_roots)) < 1e-5].real)))
+
+    if len(roots) == 2:
         left_root, right_root = roots
         window = right_root - left_root
-        x = np.linspace(left_root - window * .1, right_root + window * .1, 200)
-    else:
+        left_window, right_window = left_root - window * .1, right_root + window * .1
+    elif len(roots) == 1:
         left_root = right_root = roots[0]
-        window_roots = np.roots([a, b, c - 2 if a > 0 else c + 2])
-        left_window, right_window = left_root - 2, right_root + 2
-        x = np.linspace(left_window, right_window)
+        if a == 0:
+            left_window, right_window = left_root - 2, right_root + 2
+        else:
+            left_window, right_window = np.roots([a, b, c - 2 if a > 0 else c + 2]).tolist()
+    else:
+        if b != 0:
+            center_of_parabola_y = c - b ** 2 / (4 * a)
+            center_of_parabola_x = np.unique(np.roots([a, b, c - center_of_parabola_y]).real)[0]
+            left_window, right_window = center_of_parabola_x - 2, center_of_parabola_x + 2
+        else:
+            left_window, right_window = -5, 5
+    x = np.linspace(left_window, right_window)
     y = a * np.power(x, 2) + b * x + c
 
     fig = plt.figure()
-    plt.axvline(x=left_root, color='red', linestyle='--')
-    plt.axvline(x=right_root, color='red', linestyle='--')
+    for root in roots:
+        plt.axvline(x=root, color='red', linestyle='--')
     plt.plot(x, np.zeros(len(x)), color='black')
     plt.plot(x, y)
-    plt.scatter([left_root, right_root], [0, 0], color='red')
+    plt.scatter(roots, np.zeros(len(roots)), color='red', linestyle='--')
 
     png = io.BytesIO()
     fig.savefig(png)
