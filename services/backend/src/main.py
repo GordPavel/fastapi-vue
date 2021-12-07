a7f573792768819b3c3f858c90c225bab96f6baf
GordPavel <pvgord@icloud.com>
2021-12-07 18:19:07 +0300

    make fast api server with vue SPA

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,20 +1,10 @@
-from fastapi import FastAPI
+import base64
+import io
+
+import matplotlib.pyplot as plt
+import numpy as np
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from tortoise import Tortoise
-
-from src.database.register import register_tortoise
-from src.database.config import TORTOISE_ORM
-
-
-# enable schemas to read relationship between models
-Tortoise.init_models(["src.database.models"], "models")
-
-"""
-import 'from src.routes import users, notes' must be after 'Tortoise.init_models'
-why?
-https://stackoverflow.com/questions/65531387/tortoise-orm-for-python-no-returns-relations-of-entities-pyndantic-fastapi
-"""
-from src.routes import users, notes
 
 app = FastAPI()
 
@@ -25,12 +15,54 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(users.router)
-app.include_router(notes.router)
-
-register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
-@app.get("/")
-def home():
-    return "Hello, World!"
+@app.get("/solve")
+async def solve_equation(request: Request, a: float = 0, b: float = 0, c: float = 0):
+    solved_roots = np.roots([a, b, c])
+    solved_roots = np.round(np.unique(solved_roots[np.isreal(solved_roots)]), decimals=5)
+    return {
+        "roots": solved_roots.tolist(),
+    }
+
+
+def get_plot_encoded(a: float = 0, b: float = 0, c: float = 0):
+    solved_roots = np.roots([a, b, c])
+    roots = np.sort(solved_roots[np.isreal(solved_roots)]).tolist()
+    if len(roots) == 2:
+        left_root, right_root = np.sort(solved_roots[np.isreal(solved_roots)]).tolist()
+        window = right_root - left_root
+        if window != 0:
+            x = np.linspace(left_root - window * .1, right_root + window * .1, 200)
+        else:
+            window_roots = np.roots([a, b, c - 2 if a > 0 else c + 2])
+            left_window, right_window = np.sort(window_roots[np.isreal(window_roots)]).tolist()
+            x = np.linspace(left_window, right_window)
+        y = a * np.power(x, 2) + b * x + c
+
+        fig = plt.figure()
+
+        plt.axvline(x=left_root, color='red', linestyle='--')
+        plt.axvline(x=right_root, color='red', linestyle='--')
+        plt.plot(x, np.zeros(len(x)), color='black')
+        plt.plot(x, y)
+        plt.scatter([left_root, right_root], [0, 0], color='red')
+
+        png = io.BytesIO()
+        fig.savefig(png)
+        plot_base64 = base64.b64encode(png.getvalue()).decode('ascii')
+        return plot_base64
+
+
+@app.get("/get_plot")
+async def get_plot(request: Request, a: float = 0, b: float = 0, c: float = 0):
+    plot_base64 = get_plot_encoded(a, b, c)
+    if plot_base64:
+        return {
+            'plot': plot_base64,
+            'success': True
+        }
+    else:
+        return {
+            'success': False
+        }
--- services/frontend/src/views/Home.vue
+++ services/frontend/src/views/Home.vue
@@ -1,25 +1,121 @@
 <template>
   <section>
-    <p>This site is built with FastAPI and Vue.</p>
-
-    <div v-if="isLoggedIn" id="logout">
-      <p id="logout">Click <a href="/dashboard">here</a> to view all notes.</p>
+    <div class="container">
+      <div>Hello to quadratic equations solver</div>
+      <div>Here you can enter the coefficients of quadratic equation and this service will solve the roots</div>
+      <div>Please, enter a, b, c coefficients of equation in form
+        <span class="equation" v-katex="'a \\times x^2 + b \\times x + c = 0'"></span>
+      </div>
+      <div v-if="error" class="alert alert-danger" role="alert">
+        Sorry, error happen. Please try again.
+      </div>
+      <form @submit.prevent="solve_equation">
+        <div style="margin: 10px">
+          <div class="row align-items-start">
+            <div class="col">
+              <label for="a-coef" class="form-label">a:</label>
+              <input type="text" id="a-coef" v-model.number="a" class="form-control" :disabled="is_loading"/>
+            </div>
+            <div class="col">
+              <label for="b-coef" class="form-label">b:</label>
+              <input type="text" id="b-coef" v-model.number="b" class="form-control" :disabled="is_loading"/>
+            </div>
+            <div class="col">
+              <label for="c-coef" class="form-label">c:</label>
+              <input type="text" id="c-coef" v-model.number="c" class="form-control" :disabled="is_loading"/>
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary" :disabled="is_loading">Solve</button>
+      </form>
+      <div v-if="roots !== null" style="margin: 10px">
+        Roots: <span>{{ roots }}</span>
+        <div>
+          <button type="submit" @click.prevent="draw_plot" v-if="plot === null" class="btn btn-primary"
+                  :disabled="is_loading">Show plot
+          </button>
+          <div v-if="plot_error" class="alert alert-danger" role="alert">
+            No real roots to show the plot. Try another coefficients.
+          </div>
+          <img v-if="plot !== null" :src="'data:image/jpeg;base64,'+plot" alt="plot"/>
+        </div>
+      </div>
     </div>
-    <p v-else>
-      <span><a href="/register">Register</a></span>
-      <span> or </span>
-      <span><a href="/login">Log In</a></span>
-    </p>
   </section>
 </template>
 <script>
+import Vue from 'vue'
+import axios from 'axios';
+import VueKatex from 'vue-katex'
 
+Vue.use(VueKatex)
 export default {
   name: 'Home',
-  computed : {
-    isLoggedIn: function() {
-      return this.$store.getters.isAuthenticated;
+  data() {
+    return {
+      is_loading: false,
+      a: 0.0,
+      b: 0.0,
+      c: 0.0,
+      roots: null,
+      plot: null,
+      error: false,
+      plot_error: false,
+    };
+  },
+  methods: {
+    async solve_equation() {
+      const comp = this
+      comp.is_loading = true
+      comp.plot = null
+      comp.error = false
+      comp.plot_error = false
+      axios
+          .get('/solve', {
+            params: {
+              'a': comp.a,
+              'b': comp.b,
+              'c': comp.c,
+            }
+          })
+          .catch(() => {
+            comp.error = true
+          })
+          .then(response => {
+            comp.roots = response.data.roots
+          })
+          .finally(() => {
+            comp.is_loading = false
+          });
+    },
+    async draw_plot() {
+      const comp = this
+      comp.is_loading = true
+      comp.error = false
+      comp.plot_error = false
+      axios
+          .get('/get_plot', {
+            params: {
+              'a': comp.a,
+              'b': comp.b,
+              'c': comp.c,
+            }
+          })
+          .catch(() => {
+            comp.error = true
+          })
+          .then(response => {
+            if (response.data.success) {
+              comp.plot = response.data.plot
+            } else {
+              comp.plot_error = true
+            }
+          })
+          .finally(() => {
+            comp.is_loading = false
+          });
     }
   },
+
 }
 </script>
