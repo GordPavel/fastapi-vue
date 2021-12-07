4d85f3bd54d30031be78b822b83f75d64a00d954
GordPavel <pvgord@icloud.com>
2021-12-07 20:32:05 +0300

    fix plotter

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,8 +1,9 @@
 import base64
 import io
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -29,29 +30,30 @@ async def solve_equation(request: Request, a: float = 0, b: float = 0, c: float 
 def get_plot_encoded(a: float = 0, b: float = 0, c: float = 0):
     solved_roots = np.roots([a, b, c])
     roots = np.sort(solved_roots[np.isreal(solved_roots)]).tolist()
-    if len(roots) == 2:
-        left_root, right_root = np.sort(solved_roots[np.isreal(solved_roots)]).tolist()
+    if len(roots) == 0:
+        return
+    elif len(roots) == 2:
+        left_root, right_root = roots
         window = right_root - left_root
-        if window != 0:
-            x = np.linspace(left_root - window * .1, right_root + window * .1, 200)
-        else:
-            window_roots = np.roots([a, b, c - 2 if a > 0 else c + 2])
-            left_window, right_window = np.sort(window_roots[np.isreal(window_roots)]).tolist()
-            x = np.linspace(left_window, right_window)
-        y = a * np.power(x, 2) + b * x + c
+        x = np.linspace(left_root - window * .1, right_root + window * .1, 200)
+    else:
+        left_root = right_root = roots[0]
+        window_roots = np.roots([a, b, c - 2 if a > 0 else c + 2])
+        left_window, right_window = left_root - 2, right_root + 2
+        x = np.linspace(left_window, right_window)
+    y = a * np.power(x, 2) + b * x + c
 
-        fig = plt.figure()
+    fig = plt.figure()
+    plt.axvline(x=left_root, color='red', linestyle='--')
+    plt.axvline(x=right_root, color='red', linestyle='--')
+    plt.plot(x, np.zeros(len(x)), color='black')
+    plt.plot(x, y)
+    plt.scatter([left_root, right_root], [0, 0], color='red')
 
-        plt.axvline(x=left_root, color='red', linestyle='--')
-        plt.axvline(x=right_root, color='red', linestyle='--')
-        plt.plot(x, np.zeros(len(x)), color='black')
-        plt.plot(x, y)
-        plt.scatter([left_root, right_root], [0, 0], color='red')
-
-        png = io.BytesIO()
-        fig.savefig(png)
-        plot_base64 = base64.b64encode(png.getvalue()).decode('ascii')
-        return plot_base64
+    png = io.BytesIO()
+    fig.savefig(png)
+    plot_base64 = base64.b64encode(png.getvalue()).decode('ascii')
+    return plot_base64
 
 
 @app.get("/get_plot")
